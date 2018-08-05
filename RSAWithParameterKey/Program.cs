e0c6b5fd52a84ea61600f923f752ef9161f9e2b1
manfered <35757736+manfered@users.noreply.github.com>
2018-08-05 17:11:09 -0600

    Add project files.

---
+++ RSAWithParameterKey/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSAWithParameterKey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rsaParams = new RSAWithParameterKey();
+            const string message = "Message to Encrypt";
+
+            rsaParams.AssignNewKey();
+
+            var encrypted = rsaParams.EncryptData(Encoding.UTF8.GetBytes(message));
+            var decrypted = rsaParams.DecryptData(encrypted);
+
+            Console.WriteLine("RSA Encryption in Memory");
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine();
+            Console.WriteLine($"Message text = {message}");
+            Console.WriteLine($"Encrypted text = {Convert.ToBase64String(encrypted)}");
+            Console.WriteLine($"Decrypted text = {Encoding.UTF8.GetString(decrypted)}");
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ RSAWithParameterKey/RSAWithParameterKey.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace RSAWithParameterKey
+{
+    public class RSAWithParameterKey
+    {
+        private RSAParameters _publicKey;
+        private RSAParameters _privateKey;
+
+        public void AssignNewKey()
+        {
+            using (var rsa = new RSACryptoServiceProvider(2048))
+            {
+                rsa.PersistKeyInCsp = false;
+                _publicKey = rsa.ExportParameters(false);
+                _privateKey = rsa.ExportParameters(true);
+            }
+        }
+
+        public byte[] EncryptData(byte[] dataToEncrypt)
+        {
+            byte[] cipherbytes;
+            using (var rsa = new RSACryptoServiceProvider(2048))
+            {
+                rsa.PersistKeyInCsp = false;
+                rsa.ImportParameters(_publicKey);
+
+                cipherbytes = rsa.Encrypt(dataToEncrypt, true);
+            }
+
+            return cipherbytes;
+        }
+
+        public byte[] DecryptData(byte[] dataToDecrypt)
+        {
+            byte[] plain;
+
+            using (var rsa = new RSACryptoServiceProvider())
+            {
+                rsa.PersistKeyInCsp = false;
+
+                rsa.ImportParameters(_privateKey);
+                plain = rsa.Decrypt(dataToDecrypt, true);
+            }
+            return plain;
+        }
+    }
+}
